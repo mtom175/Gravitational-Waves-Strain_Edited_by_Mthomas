775607d0dcd1232c65bd907b02183cbfe1ba6695
Morgan Thomas <morgantom175@yahoo.com>
2026-09-10 17:57:35 +0100

    Update grav_wave_updated.py

--- grav_wave_updated.py
+++ grav_wave_updated.py
@@ -33,72 +33,27 @@ np.random.seed(84)
 stars_x = np.random.uniform(-12,12,300)
 stars_y = np.random.uniform(-12,12,300)
 
-ax1.scatter(
-    stars_x,
-    stars_y,
-    s=2,
-    color='white'
-)
+ax1.scatter(stars_x, stars_y, s=2, color='white')
 
 # Black holes
-
-bh1, = ax1.plot(
-    [],
-    [],
-    'o',
-    color = 'white',
-    markersize=30
-
-)
-
-bh2, = ax1.plot(
-    [],
-    [],
-    'o',
-    color = 'orange',
-    markersize=30
-)
+bh1, = ax1.plot([],[],'o',color = 'white',markersize=30)
+bh2, = ax1.plot([],[],'o',color = 'orange',markersize=30)
 
 
 # Trails
-
-trail1, = ax1.plot(
-    [],
-    [],
-    'white',
-    lw=1
-)
-
-trail2, = ax1.plot(
-    [],
-    [],
-    'orange',
-    lw=1
-)
+trail1, = ax1.plot([],[],'white',lw=1)
+trail2, = ax1.plot( [], [], 'orange',lw=1)
 
 # Ripple waves
-
 num_ripples = 20
-
 ripples = []
 
 for _ in range(num_ripples):
 
-    ripple, = ax1.plot(
-        [],
-        [],
-        color='white',
-        lw=1,
-        alpha=0.6
-    )
-
+    ripple, = ax1.plot([],[],color='white',lw=1,alpha=0.6)
     ripples.append(ripple)
 
-theta_circle = np.linspace(
-    0,
-    2*np.pi,
-    300
-)
+theta_circle = np.linspace(0,(2*np.pi),300)
 
 #ripple after merge
 
@@ -113,7 +68,6 @@ X2=[]
 Y2=[]
 
 frames = 500
-
 
 # ANIMATION
 
@@ -144,15 +98,8 @@ def update(frame):
     X2.append(x2)
     Y2.append(y2)
 
-    trail1.set_data(
-        X1,
-        Y1
-    )
-
-    trail2.set_data(
-        X2,
-        Y2
-    )
+    trail1.set_data(X1,Y1)
+    trail2.set_data(X2,Y2)
 
     # Gravitational wave ripples
 
@@ -165,26 +112,11 @@ def update(frame):
             x_r = r*np.cos(theta_circle)
             y_r = r*np.sin(theta_circle)
 
-            ripple.set_data(
-                x_r,
-                y_r
-                )
+            ripple.set_data(x_r,y_r)
 
-    return (
-        bh1,
-        bh2,
-        trail1,
-        trail2,
-        *ripples
-        )
+    return (bh1,bh2,trail1,trail2,*ripples)
 
-ani = FuncAnimation(
-    fig,
-    update,
-    frames=frames,
-    interval=20,
-    blit=True
-)
+ani = FuncAnimation(fig,update,frames=frames,interval=20,blit=True)
 
 plt.tight_layout()
 # saves the animation to your desktop
